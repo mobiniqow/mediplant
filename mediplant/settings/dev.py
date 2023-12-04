b3b93b8d55c06b05fdc8d19cad695067f26b42a2
mobiniqow <41586263+mobiniqow@users.noreply.github.com>
2023-12-04 09:53:12 +0330

    f

--- mediplant/settings/dev.py
+++ mediplant/settings/dev.py
@@ -5,4 +5,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR,"static")
+print(f'BASE_DIR{BASE_DIR}')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
