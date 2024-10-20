6a2357db803b1e3e3afb4cbd02d9a88ef1385c71
purya <porya.hajian79@gmail.com>
2024-10-20 16:51:26 +0330

    edit panel admin

--- sale/models.py
+++ sale/models.py
@@ -20,7 +20,7 @@ class SaleBasket(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name='کاربر')
     session_key = models.CharField(max_length=200,)
-    shop_id = models.ForeignKey(Shop,max_length=200)
+    # shop_id = models.ForeignKey(Shop,max_length=200)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
     price = models.IntegerField(validators=[MinValueValidator(0)], verbose_name='قیمت',unique=True)
     address = models.TextField(verbose_name='آدرس')
